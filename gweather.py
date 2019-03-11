31b9bff7d5b0d65a5ca201471aca6caad7e00fd2
abrius <virtualybe@gmail.com>
2019-03-11 20:18:27 +0200

    Update gweather.py

--- gweather.py
+++ gweather.py
@@ -8,10 +8,13 @@ import re
 # Your agent, can check here: http://whatsmyua.com/
 agent 			= "Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0"
 city 			= "London" 	
+
+# read source 
 source 			= urllib2.Request('https://www.google.com/search?q=weather+'+city+'',headers={'User-Agent':agent})
 opening 		= urllib2.urlopen(source) 
 array	 		= opening.read() 
 
+# find/cut only needed weather data
 updated			= re.findall('<div class="vk_gy vk_sh" id="wob_dts">(.*?)</div>',array, re.U) 
 temperature		= re.findall('<span class="wob_t" id="wob_tm" style="display:inline">(.*?)</span>',array, re.U) 
 precipitation 	        = re.findall('<span id="wob_pp">(.*?)</span>',array, re.U) 
@@ -19,6 +22,7 @@ humidity		= re.findall('<span id="wob_hm">(.*?)</span>',array, re.U)
 wind			= re.findall('<span class="wob_t" id="wob_ws">(.*?)</span>',array, re.U) 
 condition		= re.findall('<span class="vk_gy vk_sh" id="wob_dc">(.*?)</span></div></span>',array, re.U) 
 
+# data from array
 updated 		= updated[0] 
 temperature		= temperature[0]
 precipitation 	        = precipitation[0]
@@ -26,6 +30,7 @@ humidity 		= humidity[0]
 wind 			= wind[0]
 condition		= condition[0]
 
+# output
 print ("Google weather")
 print ("--------------")
 print ("City:          "+city)
